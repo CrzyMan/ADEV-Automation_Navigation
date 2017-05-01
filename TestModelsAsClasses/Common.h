62f8cf09b9e1197daca63624a8cc7c8fed9db91c
Tyler Whitehouse <whitehts@rose-hulman.edu>
2017-04-30 21:31:16 -0400

    Common
    Described the includes

--- TestModelsAsClasses/Common.h
+++ TestModelsAsClasses/Common.h
@@ -1,5 +1,16 @@
+// This file includes everything that is used by everything at the top level
+
+// Ddefines USB and initializes objects to aid communication
 #include "_forUSB.h"
+
+// Defines motor objects and constants
 #include "_forMotorControl.h"
+
+// Defines constants and data structures for readings, data, and goals
 #include "_forReadings.h"
+
+// Defines the first turn, the next turn, and how to toggle the next turn
 #include "_forTurning.h"
+
+// Keeps track of the flags
 #include "FlagProtocol.h"
