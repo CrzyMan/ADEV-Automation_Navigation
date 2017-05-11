5dc8b6dcb64ea4d13d6a17ef6caee90e5c0a405a
yatesaj <kd8caj@gmail.com>
2017-05-10 22:00:12 -0400

    End of Field Sensing implementation
    created: counter, threshold for counter and moving detection, and end
    of field reached flag
    
    implemented behaviors within turning() to sense end of field

--- TestModelsAsClasses/Picking.h
+++ TestModelsAsClasses/Picking.h
@@ -8,6 +8,10 @@
 
 class Picking : public MovementModule {
   private:
+    int counter = 0;
+    int counterThreshold = 5000;
+    float movingThreshold = 0.25;
+    boolean reachEoF = false;
     enum State {TURNING, STRAIGHT}; // Instantiate the different states within picking
     State currState = State::STRAIGHT;  // Set default state to straight
   public:
@@ -21,7 +25,6 @@ void Picking::execute() {
     case State::STRAIGHT:
       goingStraight();  // Enact the goStraight behavior
       break;
-
    case State::TURNING:
       turning();  // Enact the turning behavior
       break;
@@ -46,9 +49,32 @@ void Picking::turning(){
   GoalHandler::setDirection(Turning::next);
   //If in a row, toggle next turning, switch state to going straight
   if (robotInRow()){
+    // Toggle next turn to keep moving down field
     Turning::toggleNext();
+    // Increment number of rows picked
     Turning::rows_picked += 1;
+    // Reset counter
+    counter = 0;
+    // If robot has reached end of field
+    if(reachEoF){
+      // Toggle next turn, reset flag, send goHome request
+      Turning::toggleNext();
+      reachEoF = false;
+      FlagProtocol::setFlag(FlagProtocol::Flags::goHomeRequest);
+    }
+    // Set current state to straight
     currState = State::STRAIGHT;
+  }else{
+    // If moving and hasn't reached end of the field yet
+    if(goals[_goals_forwardSpeed] > movingThreshold && !reachEoF){
+      // Increment counter
+      counter++;      
+    }
+    // If turning for too long and counter is above threshold
+    if(counter >= counterThreshold){
+      // Has reached end of field, set flag
+      reachEoF = true;
+    }
   }
 };
 
